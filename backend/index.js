6eac73502f97dc90a374e4207cd1decb0c28441a
AfnanKhatak <afnan@channeluc.com>
2023-07-30 00:55:56 +0500

    added new mongoclient

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
+const uri = process.env.CONNECTION_STRING;
+
 
 const express = require("express");
 const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 // middleware
@@ -13,14 +16,33 @@ const User = require("./routes/user");
 
 app.use("/api/workouts", workOutPage);
 app.use("/api/user", User);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose
-  .connect(process.env.CONNECTION_STRING)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`connected to db && server is up and running on ${PORT} !!`);
+
+async function run() {
+  try {
+    await client.connect().then(()=>{
+      console.log("connected to db");
     });
-  })
-  .catch((err) => {
-    console.log("error connecting to database", err);
-  });
+    const coll = client.db('test').collection('movies');
+    const cursor = coll.find();
+    await cursor.forEach(console.dir);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+// mongoose
+//   .connect(process.env.CONNECTION_STRING)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`connected to db && server is up and running on ${PORT} !!`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("error connecting to database", err);
+//   });
